87a70554e437f3f1459c4bc9078839506844c824
gyuhang <gyuhang@amazon.com>
2021-09-01 18:46:05 -0700

    tmux: tpm, logging, restore

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,3 +1,5 @@
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 scriptPath=$(dirname $(greadlink -f ${BASH_SOURCE[0]}))
 echo -n "scriptPath: "${scriptPath}
 
@@ -6,3 +8,5 @@ ln -s ${scriptPath}/.tmux.conf $HOME/.tmux.conf
 
 # source tmux
 tmux source $HOME/.tmux.conf
+
+echo "start a tmux session and hit prefix+I to install plugins"
