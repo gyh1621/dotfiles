4cde79650fd092fcb882e8ede9e94c7686010011
gyuhang <gyuhang@amazon.com>
2021-09-03 16:54:56 -0700

    tmux: extrakto

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -3,6 +3,10 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 scriptPath=$(dirname $(greadlink -f ${BASH_SOURCE[0]}))
 echo -n "scriptPath: "${scriptPath}
 
+# extrakto
+mkdir -p ~/.config/extrakto
+ln -s ~/dotfiles/tmux/extrakto.conf ~/.config/extrakto/extrakto.conf
+
 # make link
 ln -s ${scriptPath}/.tmux.conf $HOME/.tmux.conf
 
