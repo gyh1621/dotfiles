06c59357d09d8c81e4a89bb01ca88c5968611087
gyh1621 <cngyh96@gmail.com>
2021-09-25 18:37:14 -0700

    tmux: fix setup.sh

--- tmux/setup.sh
+++ tmux/setup.sh
@@ -1,16 +1,22 @@
+set -e
+
+[ -d ~/.tmux ] && rm -rf ~/.tmux && echo "Deleted existed ~/.tmux"
+[ -f ~/.tmux.conf ] && rm ~/.tmux.conf && echo "Deleted existed ~/.tmux.conf"
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 scriptPath=$(dirname $(greadlink -f ${BASH_SOURCE[0]}))
-echo -n "scriptPath: "${scriptPath}
+echo "scriptPath: "${scriptPath}
 
 # extrakto
+[ -d ~/.config/extrakto ] && rm -rf ~/.config/extrakto
 mkdir -p ~/.config/extrakto
 ln -s ~/dotfiles/tmux/extrakto.conf ~/.config/extrakto/extrakto.conf
 
 # make link
 ln -s ${scriptPath}/.tmux.conf $HOME/.tmux.conf
 
-# source tmux
-tmux source $HOME/.tmux.conf
+# source tmux if currently inside a tmux session
+[ -z "${TMUX}" ] || tmux source $HOME/.tmux.conf
 
-echo "start a tmux session and hit prefix+I to install plugins"
+echo "Hit prefix+I inside a tmux session to install plugins"
