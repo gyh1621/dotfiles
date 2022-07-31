92c30807f05800214d3b320e05e5fe769ef3bd2c
gyh1621 <cngyh96@gmail.com>
2022-07-30 21:17:26 -0700

    test

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -20,7 +20,7 @@ function install_zsh_arch {
 function install_zsh_ubuntu {
     sudo apt update && sudo apt install zsh git curl -y
     # install go for fzf
-    cd /tmp && curl -OL https://golang.org/dl/go1.84.4.linux-amd64.tar.gz
+    cd /tmp && curl -OL https://golang.org/dl/go1.18.4.linux-amd64.tar.gz
     sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.18.4.linux-amd64.tar.gz
     export PATH=$PATH:/usr/local/go/bin
 }
