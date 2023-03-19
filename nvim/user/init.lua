0ec1d899d9c704777bce30490a6834679026f925
Yuhang Guo <gyuhang@amazon.com>
2023-03-19 00:04:29 -0700

    test

--- nvim/user/init.lua
+++ nvim/user/init.lua
@@ -115,10 +115,28 @@ return {
       "pineapplegiant/spaceduck",
       name = "spaceduck",
       branch = "dev"
-    }
+    },
+
+    "mfussenegger/nvim-jdtls", -- load jdtls on module
+    {
+      "williamboman/mason-lspconfig.nvim",
+      opts = {
+        ensure_installed = { "jdtls" },
+      },
+    },
 
   },
   lsp = {
+    setup_handlers = {
+      jdtls = function(_, opts)
+        vim.api.nvim_create_autocmd("Filetype", {
+          pattern = "java", -- autocmd to start jdtls
+          callback = function()
+            require("jdtls").start_or_attach(opts)
+          end,
+        })
+      end
+    },
     config = {
       rust_analyzer = {
         settings = {
@@ -128,7 +146,70 @@ return {
             }
           }
         }
-      }
+      },
+      jdtls = function()
+        -- use this function notation to build some variables
+        local root_markers = { ".git", "Config", {"packageInfo"} }
+        local root_dir = require("jdtls.setup").find_root(root_markers)
+
+        local ws_folders_jdtls = {}
+        if root_dir then
+          local file = io.open(root_dir .. "/.bemol/ws_root_folders")
+          if file then
+            for line in file:lines() do
+              table.insert(ws_folders_jdtls, "file://" .. line)
+            end
+            file:close()
+          end
+        end
+
+        -- calculate workspace dir
+        local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+        local workspace_dir = vim.fn.stdpath "data" .. "/site/java/workspace-root/" .. project_name
+        os.execute("mkdir -p " .. workspace_dir)
+
+        -- get the mason install path
+        local install_path = require("mason-registry").get_package("jdtls"):get_install_path()
+
+        -- get the current OS
+        local os
+        if vim.fn.has "macunix" then
+          os = "mac"
+        elseif vim.fn.has "win32" then
+          os = "win"
+        else
+          os = "linux"
+        end
+
+        -- return the server config
+        return {
+          cmd = {
+            "java",
+            "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+            "-Dosgi.bundles.defaultStartLevel=4",
+            "-Declipse.product=org.eclipse.jdt.ls.core.product",
+            "-Dlog.protocol=true",
+            "-Dlog.level=ALL",
+            "-javaagent:" .. install_path .. "/lombok.jar",
+            "-Xms1g",
+            "--add-modules=ALL-SYSTEM",
+            "--add-opens",
+            "java.base/java.util=ALL-UNNAMED",
+            "--add-opens",
+            "java.base/java.lang=ALL-UNNAMED",
+            "-jar",
+            vim.fn.glob(install_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
+            "-configuration",
+            install_path .. "/config_" .. os,
+            "-data " .. workspace_dir,
+          },
+          root_dir = root_dir,
+          init_options = {
+            workspaceFolders = ws_folders_jdtls,
+          },
+        }
+
+      end
     }
   }
 }
